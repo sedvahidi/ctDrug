05bc32809067b312e0452df7c34b66e9e3bce8e9
sedvahidi <138368833+sedvahidi@users.noreply.github.com>
2026-05-12 23:54:18 +0330

    Delete scripts directory

--- scripts/run_chemberta_smiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run ChemBERTa + SMILES
-
-python models/main/chemberta_smiles.py --mode finetune --epoch 100 --batch_size 512 
--- scripts/run_prot5_selfies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run ProtT5 + SMILES
-
-python models/main/prot5_selfies.py --mode finetune --epoch 100 --batch_size 512 --path human_cross_sel.pth
